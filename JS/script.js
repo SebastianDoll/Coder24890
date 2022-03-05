6089b4a9ca01dc81413ec9cbd3c21c5bfcc74c68
SebastianDoll <sebastianmdoll@gmail.com>
2022-03-05 17:28:51 -0300

    clase 9

--- JS/script.js
+++ JS/script.js
@@ -56,22 +56,116 @@ const productoD = new Producto("Mueble", 10, 10000, 0.8, "Madera", 4, "")
 
 const listaProductos = [productoA, productoB, productoC, productoD]
 
-let categoriaProducto = prompt("Ingrese la categoria de producto que quiere ver:")
+//let categoriaProducto = prompt("Ingrese la categoria de producto que quiere ver:")
 
-const listaSegunCategoria = listaProductos.filter(x => x.categoria == categoriaProducto)
 
-for (const producto of listaSegunCategoria) {
-    let contenedor = document.createElement("div");
-    
-    contenedor.innerHTML = `<div class="card">
-                            <p> Producto: ${producto.nombre}</p>
-                            <b> $ ${producto.precio}</b>
-                            <img src=${producto.img} class="imagen"/>
-                            <button>Comprame!</button></div>`
-  
 
-    document.body.appendChild(contenedor);
+let flagMadera = 0
+
+let madera = document.querySelector('.categoria1')
+let plastico = document.querySelector('.categoria2')
+
+//AGREGADO POST CLASE --> modifique el nombre de la funcion "renderMadera" por "renderProductos" y le agrego el parametro
+//"categoria" para no repetir la misma funcion solo por esa diferencia. Fijense que debajo esta la forma de pasar una 
+//funcion/event handler con un parametro(porque recuerden que no debe ejecutarse la funcion en el event listener, sino solo 
+// pasar una referencia)
+
+madera.addEventListener('click', function(){renderProductos("Madera")})
+plastico.addEventListener('click', function(){renderProductos("Plastico")})
+
+function renderProductos(categoria){
+    /* if(flagMadera === 0){
+        const listaSegunCategoria = listaProductos.filter(x => x.categoria == 'Madera')
+
+        let catalogo = document.querySelector('.catalogo')
+
+
+        for (const producto of listaSegunCategoria) {
+            let contenedor = document.createElement("div");
+            
+            contenedor.innerHTML = `<div class="card">
+                                    <p> Producto: ${producto.nombre}</p>
+                                    <b> $ ${producto.precio}</b>
+                                    <img src=${producto.img} class="imagen"/>
+                                    <button>Comprame!</button></div>`
+
+
+            catalogo.appendChild(contenedor);
+        }
+        flagMadera++
+    } */
+    const listaSegunCategoria = listaProductos.filter(x => x.categoria == categoria)
+
+    let catalogo = document.querySelector('.catalogo')
+
+    catalogo.innerHTML = ''
+
+    for (const producto of listaSegunCategoria) {
+        let contenedor = document.createElement("div");
+        
+        contenedor.innerHTML = `<div class="card">
+                                <p> Producto: ${producto.nombre}</p>
+                                <b> $ ${producto.precio}</b>
+                                <img src=${producto.img} class="imagen"/>
+                                <button>Comprame!</button></div>`
+
+
+        catalogo.appendChild(contenedor);
+    }
 }
+
+/* function renderPlastico(){
+    const listaSegunCategoria = listaProductos.filter(x => x.categoria == 'Plastico')
+
+    let catalogo = document.querySelector('.catalogo')
+
+    catalogo.innerHTML = ''
+
+
+    for (const producto of listaSegunCategoria) {
+        let contenedor = document.createElement("div");
+        
+        contenedor.innerHTML = `<div class="card">
+                                <p> Producto: ${producto.nombre}</p>
+                                <b> $ ${producto.precio}</b>
+                                <img src=${producto.img} class="imagen"/>
+                                <button>Comprame!</button></div>`
+
+
+        catalogo.appendChild(contenedor);
+    }
+}
+
+let catalogoNuevo = document.createElement('div') */
+
+
+
+/* let boton1 = document.querySelector('.boton1')
+
+boton1.addEventListener('click', saludar)
+
+function saludar(){
+    console.log('Bienvenido, gracias por clickear este boton')
+}
+
+let renderProductos = document.querySelector('.productos1')
+
+renderProductos.addEventListener('click', mostrarProductos)
+
+function mostrarProductos(){
+    for (const producto of listaProductos) {
+        let contenedor = document.createElement("div");
+        
+        contenedor.innerHTML = `<div class="card">
+                                <p> Producto: ${producto.nombre}</p>
+                                <b> $ ${producto.precio}</b>
+                                <img src=${producto.img} class="imagen"/>
+                                <button>Comprame!</button></div>`
+    
+
+        catalogo.appendChild(contenedor);
+    }
+} */
 
 /* for(const producto of listaProductos){
     contador++
--- JS/script3.js
+++ JS/script3.js
@@ -20,7 +20,7 @@ let listaPaises = document.getElementById("listapaises")
 
 listaPaises.insertBefore(paisNuevo, listaPaises.children[1]) */
 
-const productos = [{ id: 1, nombre: "Arroz", precio: 125 },
+/* const productos = [{ id: 1, nombre: "Arroz", precio: 125 },
  { id: 2, nombre: "Fideo", precio: 70 },
  { id: 3, nombre: "Pan" , precio: 50},
  { id: 4, nombre: "Flan" , precio: 100}];
@@ -33,7 +33,31 @@ for (const producto of productos) {
                             <b> $ ${producto.precio}</b>
                             <button>Comprame!</button>`;
     document.body.appendChild(contenedor);
+} */
+
+
+//CODIGO JS
+let input1 = document.getElementById("nombre")
+
+input1.addEventListener('change', () => {
+ console.log(input1.value)
+})
+
+let miFormulario = document.getElementById ("formulario");
+miFormulario. addEventListener ("submit", validarFormulario );
+function validarFormulario (e){
+ //Cancelamos el comportamiento del evento
+ e.preventDefault ();
+ //Obtenemos el elemento desde el cual se disparó el evento
+ let formulario = e.target
+
+ console.log(e.target)
+ //Obtengo el valor del primero hijo <input type="text">
+ console.log(formulario.children[ 0].value); 
+ //Obtengo el valor del segundo hijo <input type="number"> 
+ console.log(formulario.children[ 1].value); 
 }
+
 
 
 //console.log(titulo.innerText)
